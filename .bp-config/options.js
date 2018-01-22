37ae1d83ee0c95a64e83a80b4f0bd790979cb136
Peter Burkholder <peter.burkholder@gsa.gov>
2018-01-22 14:06:23 -0500

    PHP buildpack example

---
+++ .bp-config/options.js
@@ -0,0 +1,28 @@
+{
+  "WEB_SERVER": "httpd",
+  "ADMIN_EMAIL": "your-email@example.com",
+  "COMPOSER_INSTALL_OPTIONS": [
+    "--this-should-fail"
+  ],
+  "COMPOSER_VENDOR_DIR": "/app/htdocs/vendor",
+  "WEBDIR": "web",
+  "PHP_MODULES": [
+    "fpm",
+    "cli"
+  ],
+  "PHP_VERSION": "{PHP_70_LATEST}",
+  "PHP_EXTENSIONS": [
+    "bz2",
+    "zlib",
+    "curl",
+    "mcrypt",
+    "openssl",
+    "mbstring",
+    "pdo",
+    "pdo_mysql",
+    "apcu"
+  ],
+  "ZEND_EXTENSIONS": [
+    "opcache"
+  ]
+}
